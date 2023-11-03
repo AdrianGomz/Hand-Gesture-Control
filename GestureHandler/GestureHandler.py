01bc6331d3e0a92867e7ea5cc02c8761e3710e78
isaac <vi13150@innovaccion.mx>
2023-11-03 10:32:27 -0600

    added validations for gesture handler

--- GestureHandler/GestureHandler.py
+++ GestureHandler/GestureHandler.py
@@ -8,9 +8,24 @@ spotifyAdapter = SpotifyAdapter.SpotifyAdapter()
 
 
 def handle_gestures(gesture_list):
+    if len(gesture_list) == 0:
+        gesture_set = frozenset(["empty"])
+    else:
+        gesture_set = frozenset(gesture_list)
     global previous_gestures
-    # print(gesture_list, previous_gestures)
-    if sorted(previous_gestures) == sorted(gesture_list):
+    print(gesture_list, previous_gestures)
+    
+    previous_gestures.append(gesture_set)
+    out = None
+    if len(previous_gestures)<3:
+        return
+    if len(previous_gestures) > 3:
+            out = previous_gestures.pop(0)
+
+    if len(set(previous_gestures))>1:
+        return
+    
+    if frozenset(gesture_list)==out:
         return
     # Arduino leds
     if "L" in gesture_list and "1" in gesture_list:
@@ -21,9 +36,11 @@ def handle_gestures(gesture_list):
         ArduinoService.toggle_digital_out(4)
    
     # Arduiono vent
-    if "B" in gesture_list and "1" in gesture_list:
+    if "A" in gesture_list and "1" in gesture_list:
+        print("A  1")
         ArduinoService.toggle_digital_out(5)
-    if "B" in gesture_list and "2" in gesture_list:
+    if "A" in gesture_list and "2" in gesture_list:
+        print("A  2")
         ArduinoService.toggle_digital_out(6)
     
     # Spotify
@@ -34,5 +51,7 @@ def handle_gestures(gesture_list):
     if "C" in gesture_list and "3" in gesture_list:
         spotifyAdapter.toggle_pause_play()
     
-    previous_gestures = gesture_list
+    
+    if len(previous_gestures) > 3:
+        previous_gestures.pop(0)
     
